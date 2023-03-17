5969ad892a1c68e6626da876b3dfcfac11b7d6b6
rayesyounes <rayesyounes88@gmail.com>
2023-03-16 17:19:31 -0700

    task-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - this the main function
+ *
+ * Return: always 0
+ */
+
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle);
+	return (0):
+}
