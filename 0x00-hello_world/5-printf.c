98843ac1b11c9f8c3da42460340df66a30ad31db
rayesyounes <rayesyounes88@gmail.com>
2023-03-16 18:06:09 -0700

    task-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
