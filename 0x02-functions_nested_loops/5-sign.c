6d8f48f9989db2fc4bd0059e3aa3fcef20db446a
rayesyounes <rayesyounes88@gmail.com>
2023-03-21 10:16:44 -0700

    task-5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints signs
+ *
+ * @n: function parameter
+ *
+ * Return: 1, 0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+}
