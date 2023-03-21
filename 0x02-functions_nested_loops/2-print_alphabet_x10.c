47bf712b65ee52268758d840a6fb5649de2b6732
rayesyounes <rayesyounes88@gmail.com>
2023-03-21 07:50:05 -0700

    task-2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print _alphabet_x10 - function that print 10x alphabets
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
