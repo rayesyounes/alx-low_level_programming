2f430047368992fd940a344b24b8fb74af819cc3
rayesyounes <rayesyounes88@gmail.com>
2023-03-23 06:20:04 -0700

    task-6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: parameter
+ * Return:returns nothing
+ */
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
