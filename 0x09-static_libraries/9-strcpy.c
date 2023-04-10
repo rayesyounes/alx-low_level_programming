a4976498914f4d324876df2b4ab44669a1c630fc
rayesyounes <rayesyounes88@gmail.com>
2023-04-10 16:08:33 -0700

    task

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int o = 0;
+	int x = 0;
+
+	while (*(src + o) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < o ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[o] = '\0';
+	return (dest);
+}
