61e657d28921434fe12a6cdb13dfc4558d62ad83
rayesyounes <rayesyounes88@gmail.com>
2023-04-20 22:56:32 -0700

    task-1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(nb, n);
 
-	for (index = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nb, int));
 
