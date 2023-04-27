a8dbaaca15a844173d2ba201b63477f597550d8c
rayesyounes <rayesyounes88@gmail.com>
2023-04-27 15:32:59 -0700

    reset

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
-#ifndef _LISTS_
-#define _LISTS_
-
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct list_s - singly linked list
@@ -9,7 +8,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
@@ -20,6 +18,5 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
 
 #endif
