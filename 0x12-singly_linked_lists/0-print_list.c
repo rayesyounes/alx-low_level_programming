5ed90db40b722edcf442180585bba944c27177e3
rayesyounes <rayesyounes88@gmail.com>
2023-04-27 14:56:40 -0700

    task-0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
 /**
  * print_list - check the code for Holberton School students.
  * @h: name of the list
- * Return: the number of nodes.
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
