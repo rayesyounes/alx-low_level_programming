1bd428031a15f3e96dd9af0668b75eae36612b63
rayesyounes <rayesyounes88@gmail.com>
2023-04-27 15:01:49 -0700

    task-1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
