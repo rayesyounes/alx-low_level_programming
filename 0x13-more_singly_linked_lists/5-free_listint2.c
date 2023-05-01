8e0661759818010893c249d96df5734b57600118
rayesyounes <rayesyounes88@gmail.com>
2023-05-01 03:07:22 -0700

    task-5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *next;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		tmp = (*head)->tmp;
+		next = (*head)->next;
 		free(*head);
-		*head = tmp;
+		*head = next;
 	}
 }
