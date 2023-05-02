df8b982017564631d2aa682d71cebf06d9d35c90
rayesyounes <rayesyounes88@gmail.com>
2023-05-02 03:01:41 -0700

    task-10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: double pointer
+ * @index: index of node
+ *
+ * Return: pointer to the index node
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int node;
+	listint_t *sap, *next;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+		return (1);
+	}
+	sap = *head;
+	for (node = 0; node < index - 1; node++)
+	{
+		if (sap->next == NULL)
+			return (-1);
+		sap = sap->next;
+	}
+	next = sap->next;
+	sap->next = next->next;
+	free(next);
+	return (1);
+}
