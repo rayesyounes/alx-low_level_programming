30bdab4404cbdb7325345dd64f88cc5709bca976
rayesyounes <rayesyounes88@gmail.com>
2023-05-08 06:35:15 -0700

    task-0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,25 +11,25 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char *buffer;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	r = read(o, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	free(buffer);
+	free(buf);
 	close(o);
 
 	return (w);
