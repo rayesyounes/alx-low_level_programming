6701297c43b3cc9d2050919fb1b9e673c5500e9e
rayesyounes <rayesyounes88@gmail.com>
2023-05-08 06:33:10 -0700

    task-2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,27 +10,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			rwr = write(fd, text_content, nletters);
-		if (rwr == -1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	close(fd);
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
 
 	return (1);
 }
